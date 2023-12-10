090a9e0f255c39c0271f52c62a4829bc26e31c4b
Kent vejrup Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-12-10 18:40:20 +0100

    expanding

--- src/stage/__init__.py
+++ src/stage/__init__.py
@@ -1,3 +1,25 @@
+from stage.labels \
+    import (
+        get_label_images,
+        get_label_annotations,
+        set_label_images,
+        set_label_annotations
+)
+
+
+from stage.singletons \
+    import (
+        retrieve_validation_data,
+        retrieve_training_data,
+        update_training_data,
+        update_validation_data,
+        get_location_to_training_data,
+        set_location_to_training_data,
+        get_location_to_validation_data,
+        set_location_to_validation_data
+)
+
+
 from stage.training \
     import (
     training
@@ -15,17 +37,3 @@ from stage.dataset \
     generate_training_dataset,
     generate_validation_dataset
 )
-
-
-from stage.singletons \
-    import (
-        retrieve_validation_data,
-        retrieve_training_data,
-        update_training_data,
-        update_validation_data,
-        get_location_to_training_data,
-        set_location_to_training_data,
-        get_location_to_validation_data,
-        set_location_to_validation_data
-)
-
